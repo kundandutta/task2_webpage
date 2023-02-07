d0ff8975a86a24e0f774335ec96e7c1c792ade05
kundan kumar dutta <duttakundan921@gmail.com>
2023-02-07 09:51:14 +0530

    worked on about page and added css and js codes

--- list.js
+++ list.js
@@ -1,1 +1,7 @@
+$('li').click(function() {
+    $(this).siblings().find('a').removeClass('focus');
+    $(this).find('a').addClass('focus');
+  });
 
+  function read()
+  <a href="Profile.htm" target="_blank" class="focus">Profile</a>
