3af4ad8e9cc1fff0bb9b0988932723ad14a80040
kundan kumar dutta <duttakundan921@gmail.com>
2023-02-07 16:22:53 +0530

    cmopleted about us page

--- list.js
+++ list.js
@@ -1,7 +1,3 @@
-$('li').click(function() {
-    $(this).siblings().find('a').removeClass('focus');
-    $(this).find('a').addClass('focus');
-  });
-
-  function read()
-  <a href="Profile.htm" target="_blank" class="focus">Profile</a>
+  function read(){
+  document.getElementById("read_more").click();
+  }
